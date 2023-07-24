776fd1612f4d79b756b127d37d710370f7316cc8
joshua <joshk.5793@gmail.com>
2023-07-24 07:45:31 -0400

    css

--- fsp/frontend/src/components/Navigation/ProfileButton.js
+++ fsp/frontend/src/components/Navigation/ProfileButton.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
+  const currentUser = useSelector((state) => state.session.user)
   
   const openMenu = () => {
     if (showMenu) return;
@@ -37,14 +40,20 @@ function ProfileButton({ user }) {
         <i id="profileBars" class="fa-solid fa-bars"></i>
         <i id="profileIcon" class="fa-solid fa-user"></i>
       </button>
+
+      
       {showMenu && (
         <ul className="profile-dropdown">
-          <li>{user.firstName}</li>
+          {/* <li>{user.firstName}</li>
           <li>{user.lastName}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
+          <li>{user.email}</li> */}
+          <>
+          <LoginFormModal />
+          <br/>
+          <SignupFormModal />
+          <br/>
+          <button onClick={logout}>Log Out</button>
+          </>
         </ul>
       )}
     </>

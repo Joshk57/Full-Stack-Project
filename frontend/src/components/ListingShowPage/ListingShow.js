2ae197c43d26652222bd9c61ff8d96bc35ec9759
joshua <joshk.5793@gmail.com>
2023-07-25 23:41:04 -0400

    changes

--- frontend/src/components/ListingShowPage/ListingShow.js
+++ frontend/src/components/ListingShowPage/ListingShow.js
@@ -38,21 +38,26 @@ const ListingShow = () => {
                 </div>
             </div> */}
 
-            <div className="listing-pictures-container">
-                <div className="pic">
+            <div className="picture-container">
+                <div className="main-picture">
                     <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/05928ce6-01ff-4829-bcde-9670e2a03c22.jpeg?im_w=960"></img>
                 </div>
-                <div className="pic">
-                    <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/bd30820b-5ce2-40f7-bd81-9b6cfa32be05.jpeg?im_w=720"></img>
+                <div className="sub-picture-container">
+                    <div className="sub-picture">
+                        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/bd30820b-5ce2-40f7-bd81-9b6cfa32be05.jpeg?im_w=720"></img>
+                    </div>
+                    <div className="sub-picture">
+                        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/037b4837-ac41-43a6-8c08-8d97c606fe6d.jpeg?im_w=720"></img>
+                    </div>
                 </div>
-                <div className="pic">
-                    <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/037b4837-ac41-43a6-8c08-8d97c606fe6d.jpeg?im_w=720"></img>
-                </div>
-                <div className="pic">
-                    <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/4436c19b-0aea-48b6-8604-081652173c21.jpeg?im_w=720"></img>
-                </div>
-                <div className="pic">
-                    <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/72872607-cb6f-4740-aeee-04ff04be3646.jpeg?im_w=720"></img>
+                <div className="sub-picture-container">
+
+                    <div className="sub-picture">
+                        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/4436c19b-0aea-48b6-8604-081652173c21.jpeg?im_w=720"></img>
+                    </div>
+                    <div className="sub-picture">
+                        <img src="https://a0.muscache.com/im/pictures/miso/Hosting-578897320179146437/original/72872607-cb6f-4740-aeee-04ff04be3646.jpeg?im_w=720"></img>
+                    </div>
                 </div>
             </div>
             

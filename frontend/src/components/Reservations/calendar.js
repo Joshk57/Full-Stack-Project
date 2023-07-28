be7b47e4fff5e13d0011372f7d3f1255f7b73047
joshua <joshk.5793@gmail.com>
2023-07-28 11:49:28 -0400

    css

--- frontend/src/components/Reservations/calendar.js
+++ frontend/src/components/Reservations/calendar.js
@@ -1,19 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { DateRangePicker, isInclusivelyAfterDay } from "react-dates";
+import { useDispatch, useSelector } from "react-redux"
+import { useParams } from "react-router-dom";
+import { getListing,fetchListing } from "../../store/listings";
 import "react-dates/initialize";
 import "react-dates/lib/css/_datepicker.css";
 import "./calendar.css";
 import dayjs from "dayjs";
 
 const DatePicker = () => {
+  const dispatch = useDispatch()
+  const {listingId} = useParams()
+  const listing = useSelector(getListing(listingId))
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   const [focusedInput, setFocusedInput] = useState(null);
+  
+  useEffect(()=> {
+    dispatch(fetchListing(listingId))
+  },[listingId])
 
   const isOutsideRange = (day) => {
     const today = new Date();
     return day.isBefore(today); 
   };
+
+  const calculateNumberOfNights = () => {
+    if (startDate && endDate) {
+      const start = dayjs(startDate);
+      const end = dayjs(endDate);
+      const numNights = end.diff(start, "day");
+      return numNights;
+    }
+    return 0;
+  };
+
+  const totalPrice = () => {
+    return (listing.price * calculateNumberOfNights())
+  }
 
   const handleReserve = () => {
 
@@ -21,6 +45,16 @@ const DatePicker = () => {
 
   return (
     <div className="App">
+      <div className="price-night">
+        <span>${listing.price} </span>night
+      </div>
+      <div className="date-range-picker-wrapper">
+        <div className="check-in-text">
+          CHECK IN
+        </div>
+        <div className="check-out-text">
+          CHECK OUT
+        </div>
       <DateRangePicker
         startDate={startDate}
         startDateId="startDate"
@@ -32,16 +66,27 @@ const DatePicker = () => {
         }}
         focusedInput={focusedInput}
         onFocusChange={(focused) => setFocusedInput(focused)}
-        showDefaultInputIcon
+        // showDefaultInputIcon
         hideKeyboardShortcutsPanel
         numberOfMonths={2}
         orientation="horizontal"
         minimumNights={1}
         isOutsideRange={isOutsideRange} 
-      />
+        />
+      </div> 
+
       <button className="reserve-btn" onClick={handleReserve}>
         Reserve
       </button>
+      <div className="price-calculation-container">
+        <div className="price-calculation">
+          ${listing.price} x {calculateNumberOfNights()} nights
+        <span>$ {totalPrice()}</span>
+        </div>
+        <div>
+
+        </div>
+      </div>
     </div>
   );
 };

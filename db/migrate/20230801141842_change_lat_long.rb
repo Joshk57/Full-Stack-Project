b8792a2e599f5356db27e1be8ac0a9e288d03ba7
joshua <joshk.5793@gmail.com>
2023-08-09 16:53:30 -0400

    changes

--- db/migrate/20230801141842_change_lat_long.rb
+++ db/migrate/20230801141842_change_lat_long.rb
@@ -1,6 +1,15 @@
 class ChangeLatLong < ActiveRecord::Migration[7.0]
   def change
-    change_column :listings, :latitude, :float, null: false
-    change_column :listings, :longitude, :float, null: false
+  #   change_column :listings, :latitude, :float, null: false
+  #   change_column :listings, :longitude, :float, null: false
   end
+  # def up
+  #   # change_column :listings, :latitude, :float, null: false
+  #   # change_column :listings, :longitude, :float, null: false
+  # end
+
+  # def down
+  #   # change_column :listings, :latitude, :float, null: true
+  #   # change_column :listings, :longitude, :float, null: true
+  # end
 end
--- db/migrate/20230801142112_change_lat_lng.rb
+++ db/migrate/20230801142112_change_lat_lng.rb
@@ -1,5 +1,6 @@
 class ChangeLatLng < ActiveRecord::Migration[7.0]
   def change
-
+    remove_column :listings, :latitude, :float
+    remove_column :listings, :longitude, :float
   end
 end

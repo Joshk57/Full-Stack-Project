90884412193b6e8582521a35a8799cae6f2ff429
Joshua Kim <joshk.5793@gmail.com>
2024-02-02 23:27:59 -0500

    practicing reviews

---
+++ frontend/src/store/reviews.js
@@ -0,0 +1,7 @@
+import csrfFetch from "./csrf";
+
+export const RECEIVE_REVIEW = 'reviews/receiveReview'
+export const RECEIVE_REVIEWS = 'reviews/receiveReviews'
+export const REMOVE_REVIEWS = 'reviews/removeReviews'
+
+

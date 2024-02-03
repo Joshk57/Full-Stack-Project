afcfe75e8df6e34564543ad874d25b9bb5382770
Joshua Kim <joshk.5793@gmail.com>
2024-02-02 23:42:09 -0500

    reviews store

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -5,3 +5,39 @@ export const RECEIVE_REVIEWS = 'reviews/receiveReviews'
 export const REMOVE_REVIEWS = 'reviews/removeReviews'
 
 
+const receiveReview = (review) => {
+    return {
+        type: RECEIVE_REVIEW,
+        review
+    }
+} 
+
+const receiveReviews = (reviews) => {
+    return {
+        type: RECEIVE_REVIEWS,
+        reviews
+    }
+} 
+
+const removeReviews = (reviewId) => {
+    return {
+        type: REMOVE_REVIEW,
+        reviewId
+    }
+} 
+
+export const getReview = (reviewId) => (state) => {
+    if (state && state.reviews) {
+        return state.reviews[reviewId]
+    } else {
+        return null
+    }
+}
+
+export const getReviews = (state) => {
+    if (state && state.reviews) {
+        return Object.values(state.reviews)
+    } else {
+        return []
+    }
+}

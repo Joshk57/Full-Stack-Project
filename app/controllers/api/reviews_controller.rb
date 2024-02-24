8b005f060be74b22949982549537019461c4366e
Joshua Kim <joshk.5793@gmail.com>
2024-02-24 14:09:33 -0500

    changes to reviews

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -2,19 +2,20 @@ class Api::ReviewsController < ApplicationController
 
 # fix VSCode and then do migrations
 
-
-    def show
-        @review = Review.find_by(id: params[:id])
-
-        if @review
-            render :review
-        else
-            render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
-        end
+    def index
+        @reviews = Review.all
+        # add views in order to render
+        render 'api/reviews/index'
+        
     end
 
     def create
-        @review = 
+        @review = Review.new(review_params)
+        if @review.save
+            render 'api/reviews/show'
+        else
+            render json: { errors: @review.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
     # check conditions
